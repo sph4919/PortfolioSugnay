c9c6711104deac4c750d50db52dc20a7a2134f6a
sph4919 <patelsugnaykumar492003@gmail.com>
2024-10-31 15:43:58 -0600

    Removing toggle issue

--- js/eventHandler.js
+++ js/eventHandler.js
@@ -2,7 +2,7 @@
 
 let count = 1;
 
-function toggleSetter(event)
+function toggleSetter()
 {
 
 
@@ -12,31 +12,72 @@ function toggleSetter(event)
 
   let navigation = document.getElementById("hideNavigation");
   navigation.style.display = "none";
+  
 
   let blank = document.getElementById("hideblank");
   blank.style.display = "none";
+  blank.style.transition = "0.7s";
 
   let content = document.getElementById("ContentSection");
-  content.style.width = "98vw";
+  content.style.width = "96vw";
+  content.style.marginLeft = "35px";
   count++;
+
+  let navbutton = document.getElementById("toggle");
+  navbutton.style.transition = "1s";
+  navbutton.style.height = "30px";
   
 
-  let Name = event.targetName;
-  console.log(Name)
+  
   }
   else
   {
     let navigation = document.getElementById("hideNavigation");
     navigation.style.display = "block";
+    navigation.style.transition = "0.7s"
   
     let blank = document.getElementById("hideblank");
     blank.style.display = "block";
-    console.log(count);
+    blank.style.transition = "0.7s";
+
     let content = document.getElementById("ContentSection");
     content.style.width = "72vw";
+    content.style.marginLeft = "0px";
+
+    let navbutton = document.getElementById("toggle");
+    navbutton.style.transition = "1s";
+    navbutton.style.height = "45px";
+
+    
   
     count++;
   }
 
 
+}
+
+
+function scrollAbout()
+{
+    document.getElementById('AboutSC').scrollIntoView({ behavior: 'smooth' });
+}
+function scrollEducation()
+{
+    document.getElementById('EducationSC').scrollIntoView({ behavior: 'smooth' });
+}
+function scrollPS()
+{
+    document.getElementById('PSSC').scrollIntoView({ behavior: 'smooth' });
+}
+function scrollCerti()
+{
+    document.getElementById('CertiSC').scrollIntoView({ behavior: 'smooth' });
+}
+function scrollFuture()
+{
+    document.getElementById('FutureSC').scrollIntoView({ behavior: 'smooth' });
+}
+function scrollWE()
+{
+    document.getElementById('WESC').scrollIntoView({ behavior: 'smooth' });
 }
